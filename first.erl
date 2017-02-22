d18d06b258cc43567a9fc0db56e1805b295cbed2
Sean Omlor <somlor@eml.cc>
2017-02-21 21:37:46 -0500

    second exercises

--- first.erl
+++ first.erl
@@ -1,12 +1,15 @@
 -module(first).
 -export([area/3, double/1, mult/2]).
 
+% calculate hypotenuse of right triangle
 mult(X, Y) ->
   X * Y.
 
+% double a number
 double(X) ->
   mult(2, X).
 
+% calculate area of a triangle
 area(A, B, C) ->
   S = (A + B + C) / 2,
   math:sqrt(S * (S - A) * (S - B) * (S - C)).
---
+++ second.erl
@@ -0,0 +1,18 @@
+-module(second).
+-export([area/2, hyp/2, perim/2]).
+
+% square a number
+square(X) ->
+  X * X.
+
+% calculate hypotenuse of right triangle
+hyp(A, B) ->
+  math:sqrt(square(A) + square(B)).
+
+% calculate perimeter of right triangle
+perim(A, B) ->
+  (A + B) + hyp(A, B).
+
+% calculate area of right triangle
+area(A, B) ->
+  (A * B) / 2.
