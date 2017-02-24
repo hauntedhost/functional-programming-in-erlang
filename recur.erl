7691271bacb2a20c568450763899c05a2322c311
Sean Omlor <somlor@eml.cc>
2017-02-23 21:29:28 -0500

    extract and expand on tail recursion exercises

--- recur.erl
+++ recur.erl
@@ -4,25 +4,13 @@
   fib/1,
   fibs1/1,
   fibs2/1,
-  pieces/1,
-  tail_fac/1,
-  tail_fib/1,
-  tail_fibs/1,
-  tail_pieces/1
+  pieces/1
 ]).
 
 fac(0) ->
   1;
 fac(N) when N > 0 ->
   fac(N - 1) * N.
-
-tail_fac(N) ->
-  tail_fac(N, 1).
-
-tail_fac(0, Acc) ->
-  Acc;
-tail_fac(N, Acc) when N > 0 ->
-  tail_fac(N - 1, N * Acc).
 
 fib(0) ->
   nil;
@@ -32,18 +20,6 @@ fib(2) ->
   1;
 fib(N) ->
   fib(N - 2) + fib(N - 1).
-
-tail_fib(N) ->
-  tail_fib(N, 0, 1).
-
-tail_fib(0, _, _) ->
-  nil;
-tail_fib(1, Y, _) ->
-  Y;
-tail_fib(2, _, Z) ->
-  Z;
-tail_fib(N, Y, Z) ->
-  tail_fib(N - 1, Z, Y + Z).
 
 fibs1(0) ->
   [];
@@ -67,30 +43,9 @@ fibs2(N) when N > 3 ->
   [Y, Z] = lists:nthtail(length(Fibs) - 2, Fibs),
   Fibs ++ [Y + Z].
 
-tail_fibs(N) ->
-  tail_fibs(N, [0, 1, 1]).
-
-tail_fibs(3, Fibs) ->
-  Fibs;
-tail_fibs(N, Fibs) when N < 3 ->
-  lists:sublist(Fibs, N);
-tail_fibs(N, Fibs) when N > 3 ->
-  [Y, Z] = lists:nthtail(length(Fibs) - 2, Fibs),
-  tail_fibs(N - 1, Fibs ++ [Y + Z]).
-
 pieces(0) ->
   0;
 pieces(1) ->
   2;
 pieces(N) ->
   N + pieces(N - 1).
-
-tail_pieces(N) ->
-  tail_pieces(N, 2).
-
-tail_pieces(0, _) ->
-  0;
-tail_pieces(1, P) ->
-  P;
-tail_pieces(N, P) ->
-  tail_pieces(N - 1, N + P).
---
+++ tail.erl
@@ -0,0 +1,91 @@
+-module(tail).
+-export([
+  fac/1,
+  fib/1,
+  fibs1/1,
+  fibs2/1,
+  perfect1/1,
+  perfect2/1,
+  pieces/1
+]).
+
+% --
+
+fac(N) ->
+  fac(N, 1).
+
+fac(0, F) ->
+  F;
+fac(N, F) when N > 0 ->
+  fac(N - 1, N * F).
+
+% --
+
+fib(N) ->
+  fib(N, 0, 1).
+
+fib(0, _, _) ->
+  nil;
+fib(1, Y, _) ->
+  Y;
+fib(N, Y, Z) ->
+  fib(N - 1, Z, Y + Z).
+
+% --
+
+fibs1(N) ->
+  fibs1(N, [0, 1, 1]).
+
+fibs1(3, Fibs) ->
+  Fibs;
+fibs1(N, Fibs) when N < 3 ->
+  lists:sublist(Fibs, N);
+fibs1(N, Fibs) when N > 3 ->
+  [Y, Z] = lists:nthtail(length(Fibs) - 2, Fibs),
+  fibs1(N - 1, Fibs ++ [Y + Z]).
+
+% return list of n fibs, reversing final list just once
+
+fibs2(N) ->
+  fibs2(N, [1, 1, 0]).
+
+fibs2(3, Fibs) ->
+  lists:reverse(Fibs);
+fibs2(N, Fibs) when N < 3 ->
+  lists:sublist(lists:reverse(Fibs), N);
+fibs2(N, Fibs = [Z, Y | _]) when N > 3 ->
+  fibs2(N - 1, [Y + Z | Fibs]).
+
+% --
+
+pieces(N) ->
+  pieces(N, 2).
+
+pieces(0, _) ->
+  0;
+pieces(1, P) ->
+  P;
+pieces(N, P) ->
+  pieces(N - 1, N + P).
+
+% returns a boolean which indicates whether or not the number is perfect
+
+perfect1(N) ->
+  perfect1(N, _Sum = 0, _Div = 1).
+
+perfect1(N, Sum, Div) when Div == N ->
+  Sum == N;
+perfect1(N, Sum, Div) when N rem Div == 0 ->
+  perfect1(N, Sum + Div, Div + 1);
+perfect1(N, Sum, Div) ->
+  perfect1(N, Sum, Div + 1).
+
+perfect2(N) ->
+  perfect2(N, _Div = 1, _Sum = 0).
+
+perfect2(N, _Div = N, Sum) ->
+  N == Sum;
+perfect2(N, Div, Sum) when N rem Div == 0 ->
+  perfect2(N, Div + 1, Sum + Div);
+perfect2(N, Div, Sum) ->
+  perfect2(N, Div + 1, Sum).
