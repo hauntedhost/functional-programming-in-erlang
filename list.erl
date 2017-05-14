7dec91d99f2ca3712b0997f2ae9b135a1e16cd35
Sean Omlor <somlor@eml.cc>
2017-05-13 21:04:06 -0400

    take and split

--- list.erl
+++ list.erl
@@ -7,9 +7,12 @@
   median/1,
   modes/1,
   product/1,
+  split_t/2,
   sum/1,
   sum_t/1,
   tail/1,
+  take/2,
+  take_t/2,
   weird_case/1
 ]).
 
@@ -67,3 +70,27 @@ modes([], Modes) -> Modes;
 modes([X | Xs], Modes) ->
   Count = maps:get(X, Modes, 0) + 1,
   modes(Xs, maps:put(X, Count, Modes)).
+
+% given integer and list, returns slice of N elements
+-spec take(integer(), [T]) -> [T].
+take(0, _) -> [];
+take(_N, []) -> [];
+take(N, [X | Xs]) when N > 0 -> [X | take(N - 1, Xs)].
+
+% tail call optimized take
+-spec take_t(integer(), [T]) -> [T].
+take_t(N, List) -> take_t(N, List, []).
+take_t(0, _, Accum) -> lists:reverse(Accum);
+take_t(_N, [], Accum) -> lists:reverse(Accum);
+take_t(N, [X | Xs], Accum) when N > 0 ->
+  take_t(N - 1, Xs, [X | Accum]).
+
+% given integer and list, returns tuple with
+% slice of N elements on left and remainder on right
+-spec split_t(integer(), [T]) -> {[T], [T]}.
+split_t(N, Right) -> split_t(N, [], Right).
+split_t(0, Left, Right) -> {lists:reverse(Left), Right};
+split_t(N, Left, [X | Xs]) ->
+  split_t(N - 1, [X | Left], Xs);
+split_t(_N, _Left, []) ->
+  erlang:error(badarg).
